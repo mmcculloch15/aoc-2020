fcb17a26d1ed591e01ce7877058dbcbc36d76818
Mitchell McCulloch <mitchell.mcculloch@rangle.io>
2020-12-02 09:13:12 -0500

    Day 2 complete

---
+++ src/day2/index.js
@@ -0,0 +1,47 @@
+const { readInput } = require('../utils')
+
+const prepareInput = (rawInput) => rawInput
+
+// TODO: Use a regex here
+const input = prepareInput(readInput()).split('\n').map(line => ({
+  minTimes: line.split('-')[0],
+  maxTimes: line.split('-')[1].split(' ')[0],
+  character: line.split(' ')[1][0],
+  password: line.split(' ')[2]
+}))
+
+const goA = (input) => {
+  let matchingPasswords = 0
+  for (const line of input) {
+    let count = 0
+    const { minTimes, maxTimes, character, password } = line
+    for (const char of password) {
+      if (char === character) count++
+    }
+
+    if (count >= minTimes && count <= maxTimes) {
+      matchingPasswords++
+    }
+  }
+  return matchingPasswords
+}
+
+const goB = (input) => {
+  let matchingPasswords = 0
+  for (const line of input) {
+    let matchingPositions = 0
+    const { minTimes: firstPos, maxTimes: secondPos, character, password } = line
+    if (password[firstPos - 1] === character) matchingPositions++
+    if (password[secondPos - 1] === character) matchingPositions++
+    if (matchingPositions === 1) matchingPasswords++
+  }
+  return matchingPasswords
+}
+
+console.time('Time')
+const resultA = goA(input)
+const resultB = goB(input)
+console.timeEnd('Time')
+
+console.log('Solution to part 1:', resultA)
+console.log('Solution to part 2:', resultB)

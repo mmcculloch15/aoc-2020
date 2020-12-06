1459a5d889b82abf5d08c79ec15b49bed8e1bc6a
Mitchell Mcculloch <mitchell.mcculloch28@gmail.com>
2020-12-06 09:27:13 -0500

    Fix readInput function for windows

--- src/utils/readInput.js
+++ src/utils/readInput.js
@@ -1,12 +1,14 @@
-const { readFileSync } = require("fs")
-const getCallerFile = require("get-caller-file")
+const { readFileSync } = require('fs')
+const getCallerFile = require('get-caller-file')
 
 const readInput = () => {
+  const fileSeparator = process.platform === 'win32' ? '\\' : '/'
+
   const file = getCallerFile()
-    .split("/")
+    .split(fileSeparator)
     .slice(0, -1)
-    .concat("input.txt")
-    .join("/")
+    .concat('input.txt')
+    .join(fileSeparator)
 
   return readFileSync(file).toString()
 }

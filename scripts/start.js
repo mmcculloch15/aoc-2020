26bf7234f0f80926752cfd32fabdc706b5557825
Mitchell McCulloch <mitchell.mcculloch@bluejlegal.com>
2021-12-04 11:54:39 -0500

    modified script to put new files into a 2021 folder

--- scripts/start.js
+++ scripts/start.js
@@ -2,15 +2,16 @@ const { spawn, execSync } = require('child_process')
 const { readdirSync } = require('fs')
 const { cp } = require('shelljs')
 
+const YEAR = '2021'
 const day = process.argv[2]
-const days = readdirSync('./src')
+const days = readdirSync(`./src/${YEAR}`)
 
 if (!days.includes(day)) {
-  console.log(`Creating file structure for ${day}...`)
-  cp('-r', 'src/template', `src/${day}`)
+  console.log(`Creating file structure ${day} of ${YEAR}`)
+  cp('-r', 'src/template', `src/${YEAR}/${day}`)
 }
 
-spawn('nodemon', [`src/${day}/index.js`], {
+spawn('nodemon', [`src/${YEAR}/${day}/index.js`], {
   stdio: 'inherit',
   shell: true
 })
